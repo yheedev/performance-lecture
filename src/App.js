9e4c1663c5a31d293bbaaa51f849c92bf5d4418a
yheedev <yheedev@gmail.com>
2025-08-31 10:48:56 +0900

    initial comiit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import ListPage from './pages/ListPage/index';
-import ViewPage from './pages/ViewPage/index';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ListPage from "./pages/ListPage/index";
+import ViewPage from "./pages/ViewPage/index";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<ListPage />} />
-        <Route path="/view/:id" element={<ViewPage />} />
+        <Route path='/' element={<ListPage />} />
+        <Route path='/view/:id' element={<ViewPage />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+// for initial commit
